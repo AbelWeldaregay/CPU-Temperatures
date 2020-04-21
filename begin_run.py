9d29056c575a003ba4cde16488e92c3f7841c314
Abel Weldaregay <abelweldaregay@gmail.com>
2020-04-21 00:23:53 -0400

    check for units when processing cpu temps

--- begin_run.py
+++ begin_run.py
@@ -3,9 +3,10 @@ from parse_temps import parse_raw_temps
 
 def begin_run():
 	input_file_path = "./input/" + sys.argv[1]
+	includes_units = sys.argv[2] == "yes"  # set to False for files without units
 	temps = []
 	with open(input_file_path, 'r') as f:
-		temps = parse_raw_temps(f, 30, False)
+		temps = parse_raw_temps(f, 30, units=includes_units)
 		print(*temps, sep='\n') # * will unpack the list
 if __name__ == "__main__":
 	begin_run()
